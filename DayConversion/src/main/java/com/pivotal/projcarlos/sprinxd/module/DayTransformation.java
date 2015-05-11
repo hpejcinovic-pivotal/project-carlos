8710b2ee2d76ec499bb56c19a9f65314a1b01407
hpejcinovic-pivotal <hpejcinovic@pivotal.io>
2015-05-11 16:40:36 +1000

    added logging

--- DayConversion/src/main/java/com/pivotal/projcarlos/sprinxd/module/DayTransformation.java
+++ DayConversion/src/main/java/com/pivotal/projcarlos/sprinxd/module/DayTransformation.java
@@ -3,79 +3,57 @@ package com.pivotal.projcarlos.sprinxd.module;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import org.springframework.xd.reactor.Processor;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.transformer.MessageTransformationException;
 import org.springframework.xd.tuple.Tuple;
-import org.springframework.xd.tuple.TupleBuilder;
 
-import reactor.rx.Stream;
+@MessageEndpoint
+public class DayTransformation {
+	private static final DateFormat inputFormat = new SimpleDateFormat(
+			"yyyy-mm-dd hh:MM:ss");
+	private static final DateFormat outputFormat = new SimpleDateFormat("EEE");
 
-public class DayTransformation implements Processor<Tuple, TaxiRide> {
+	public DayTransformation() {
+	}
 
-    private static final DateFormat inputFormat = new SimpleDateFormat("yyyy-mm-dd hh:MM:ss");
-    private static final DateFormat outputFormat = new SimpleDateFormat("EEE");
+	@Transformer(inputChannel = "input", outputChannel = "output")
+	public String transform(Tuple taxiRide) {
+		try {
+			System.out.println("In DayTransformer recordId" + taxiRide.getString("record_id"));
+			Date dayOfWeekDate = getDateFromString(taxiRide.getString(
+					"dropoff_datetime"));
+			String pickup_datetime = taxiRide.getString("pickup_datetime");
+			String dropoff_datetime = taxiRide.getString("dropoff_datetime");
+			String pickup_Cell = taxiRide.getString("pickup_Cell");
+			String dropoff_Cell = taxiRide.getString("dropoff_Cell");
 
-    public DayTransformation() {
-    }
+			String dayOfWeek = getDayOfWeek(dayOfWeekDate);
 
+			String returnString = dayOfWeek + "," + pickup_datetime + ","
+					+ dropoff_datetime + "," + pickup_Cell + "," + dropoff_Cell;
+			System.out.println("return string:" + returnString);
+			return returnString;
+		} catch (Exception e) {
+			throw new MessageTransformationException(
+					"Unable to transform taxiRide: " + e.getMessage(), e);
+		}
+	}
 
-    @Override
-    public Stream<TaxiRide> process(Stream<Tuple> inputStream) {
-        return inputStream.map(tuple -> transformDate(tuple));
-    }
+	private static String getDayOfWeek(Date date) {
 
-    private static TaxiRide transformDate(Tuple tuple) {
-        List<Object> values = new ArrayList<>(tuple.getValues());
-        List<String> fieldNames = new ArrayList<>(tuple.getFieldNames());
-        Date date = getDateFromString(tuple.getString("dropoff_datetime"));
-        fieldNames.add("dayOfWeek");
-        values.add(getDayOfWeek(date));
-        
-        tuple = TupleBuilder.tuple().ofNamesAndValues(fieldNames, values);
-        
-        
-        TaxiRide ride = new TaxiRide();
-        	
-        ride.setDropoff_datetime(tuple.getString("dropoff_datetime"));
-    	ride.setDropoff_latitude(tuple.getString("dropoff_latitude"));
-    	ride.setDropoff_longitude(tuple.getString("dropoff_longitude"));
-    	ride.setFare_amount(tuple.getString("fare_amount"));
-    	ride.setHack_licence(tuple.getString("hack_licence"));
-    	ride.setMedallion(tuple.getString("medallion"));
-    	ride.setMta_tax(tuple.getString("mta_tax"));
-    	ride.setPayment_type(tuple.getString("payment_type"));
-    	ride.setPickup_datetime(tuple.getString("pickup_datetime"));
-    	ride.setPickup_latitude(tuple.getString("pickup_latitude"));
-    	ride.setPickup_longitude(tuple.getString("pickup_longitude"));
-    	ride.setSurcharge(tuple.getString("surcharge"));
-    	ride.setTip_amount(tuple.getString("tip_amount"));
-    	ride.setTolls_amount(tuple.getString("tolls_amount"));
-    	ride.setTotal_amount(tuple.getString("total_amount"));
-    	ride.setTrip_distance(tuple.getString("trip_distance"));
-    	ride.setTrip_tim_in_secs(tuple.getString("trip_tim_in_secs"));
-    	ride.setDropoff_Cell(tuple.getString("dropoff_Cell"));
-    	ride.setPickup_Cell(tuple.getString("pickup_Cell"));
-    	ride.setDayOfWeek(tuple.getString("dayOfWeek"));
-    	
-    	return ride;
-        
-    }
+		return outputFormat.format(date);
+	}
 
-    private static String getDayOfWeek(Date date) {
-
-        return outputFormat.format(date);
-    }
-
-    private static Date getDateFromString(String dateString) {
-        try {
-            return inputFormat.parse(dateString);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	private static Date getDateFromString(String dateString) {
+		try {
+			return inputFormat.parse(dateString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
